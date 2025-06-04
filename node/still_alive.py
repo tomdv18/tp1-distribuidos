58d9cbfc3b10636991911393ee759d8ae7fb3565
Pedro Grin <grinpedro99@gmail.com>
2025-06-04 02:42:45 -0300

    health checker done

--- node/still_alive.py
+++ node/still_alive.py
@@ -1,10 +1,12 @@
 import os
 import socket
 from multiprocessing import Process, Event
+import random
 
 class StillAlive:
     def __init__(self):
-        self.port = int(os.environ.get("HEALTH_CHECK_PORT", 5000))
+        self.port = int(os.environ.get("HEALTH_CHECK_PORT", 8000))
+        self.hc_id = os.environ.get("HEALTH_CHECK_ID", "1")
         self.node_id = os.environ.get("CONTAINER_NAME", os.environ.get("HOSTNAME", "unknown"))
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._stop_event = Event()
@@ -14,8 +16,8 @@ class StillAlive:
         try:
             while not self._stop_event.is_set():
                 try:
-                    self.sock.sendto(self.node_id.encode(), ("health_checker", self.port))
-                    print(f"Enviado heartbeat de {self.node_id} al puerto {self.port}")
+                    self.sock.sendto(self.node_id.encode(), (f"health_checker_{self.hc_id}", self.port))
+                    print(f"Enviado heartbeat de {self.node_id} al puerto {self.port} de health_checker_{self.hc_id}")
                 except Exception as e:
                     print(f"Error enviando heartbeat: {e}")
                 self._stop_event.wait(5)
